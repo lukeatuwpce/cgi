d3472687cba142a00ba46e9e9954277dcacd3274
Luke Rider <luke@rider.us>
2018-07-22 17:36:03 -0700

    cgi_sums.py -- mostly from class notes

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,15 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist('operand')
+
+try:
+    total = sum(map(int, operands))
+    body = f"Total: {total}"
+except (ValueError, TypeError):
+    body = "sum failed: provide integer operands."
+
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print(body)
